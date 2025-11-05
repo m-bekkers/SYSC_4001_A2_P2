87fa3bae539f75f2bfc9a52c26880ddcba833c94
LaavanyaNayar <laavanya.nayar@gmail.com>
2025-11-05 07:59:25 -0500

    Made the two process independent from each other

--- concurrent_processes.cpp
+++ concurrent_processes.cpp
@@ -4,38 +4,60 @@
 
 int main() {
 
-	pid_t pid;
-	pid =fork();
+	pid_t pid1,pid2;
 	
-	if(pid<0){
+	pid1 =fork();
+	
+	if(pid1<0){
 		//error
 		perror("fork failed");
 		return 1;
 		
-		}
-	if(pid==0){
+	}
+	
+	if(pid1==0){
 		// child process
 		int counter=0;
 		while(1){
 		
-			printf("Child (PID %d): counter = %d \n",getpid(),counter++);
+			printf("Process 1 (PID %d): counter = %d \n",getpid(),counter++);
 			sleep(1);
 								
-			}
 		}
-		
+	}
+	
 	else{
 		// parent process
-		int counter=0;
-		while(1){
+		pid2 =fork();
 		
-			printf("Parent (PID %d): counter = %d \n",getpid(),counter++);
-			sleep(1);
-								
+		if(pid2<0){
+			//error
+			perror("fork failed");
+			return 1;
+			
+		}
+			
+		if(pid2==0){
+			// child process
+			int counter=0;
+			while(1){
+			
+				printf("Process 2 (PID %d): counter = %d \n",getpid(),counter++);
+				sleep(1);
+									
 			}
 		}
+			
+		
+		else{
+			printf("Parent PID: %d, Child 1 PID: %d, Child 2 PID: %d\n",getpid(), pid1, pid2);
+			return 0; // parrent exits.
+					
+		}	
+	}
+		
 	return 0;
 	
-	}
+}
 		
 	
