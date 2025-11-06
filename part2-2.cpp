742cede2dd5769161a60840e664780a8d618c73b
matthedeb <matthedeb@hotmail.com>
2025-11-05 20:03:21 -0500

    added part 2 question 2

---
+++ part2-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+
+int main() {
+    pid_t pid;
+    pid = fork();
+
+    int counter = 0;
+    std::string message = "";
+
+    // check fork went OK
+    switch (pid) {
+    case -1:
+        std::cout << "Error: Fork Failed" << std::endl;
+        exit(1);
+        break;
+    case 0:
+        std::cout << "Process is child, proceeding..." << std::endl;
+        message = "Child";
+        break;
+    
+    default:
+        std::cout << "Process is parent, proceeding..." << std::endl;
+        message = "Parent";
+    }
+
+    int pid_num = getpid();
+
+    std::cout << message << " process PID(" << pid_num << ") cycle number: " << counter << std::endl;
+    
+    while (1) {
+    
+        if (pid == 0) {
+            counter--;
+        }
+        else {
+            counter++;
+        }
+
+        std::cout << message << " process PID(" << pid_num << ") cycle number: " << counter << std::endl;
+        
+        sleep(1);
+    }
+}
